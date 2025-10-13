9204cd0c0ffe49147f7d67f4ab8ad60ce96e07fd
Eugen Dryl <pickthepill@gmail.com>
2025-10-13 11:23:58 +0300

    isomorphicStrings

---
+++ src/main/kotlin/IsomorphicStrings.kt
@@ -0,0 +1,77 @@
+/**
+ * 205. Isomorphic Strings
+ *
+ * Given two strings s and t, determine if they are isomorphic.
+ *
+ * Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+ *
+ * All occurrences of a character must be replaced with another character while preserving the order of characters.
+ * No two characters may map to the same character, but a character may map to itself.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "egg", t = "add"
+ *
+ * Output: true
+ *
+ * Explanation:
+ *
+ * The strings s and t can be made identical by:
+ *
+ * Mapping 'e' to 'a'.
+ * Mapping 'g' to 'd'.
+ * Example 2:
+ *
+ * Input: s = "foo", t = "bar"
+ *
+ * Output: false
+ *
+ * Explanation:
+ *
+ * The strings s and t can not be made identical as 'o' needs to be mapped to both 'a' and 'r'.
+ *
+ * Example 3:
+ *
+ * Input: s = "paper", t = "title"
+ *
+ * Output: true
+ *
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 5 * 104
+ * t.length == s.length
+ * s and t consist of any valid ascii character.
+ *
+ * */
+
+fun main() {
+    println(isIsomorphic("egg", "add"))
+    println(isIsomorphic("foo", "bar"))
+    println(isIsomorphic("paper", "title"))
+    println(isIsomorphic("papers", "titles"))
+}
+
+private fun isIsomorphic(s: String, t: String): Boolean {
+    var i = 0
+    var mappingsTtoS = hashMapOf<Char, Char>()
+    var mappingsStoT = hashMapOf<Char, Char>()
+    while (i < s.length) {
+        if (mappingsTtoS[t[i]] == null) {
+            mappingsTtoS[t[i]] = s[i]
+        }
+        if (mappingsStoT[s[i]] == null) {
+            mappingsStoT[s[i]] = t[i]
+        }
+
+        if ((mappingsTtoS[t[i]] != s[i]) || mappingsStoT[s[i]] != t[i]) {
+            return false
+        }
+        i++
+    }
+
+    return true
+}
