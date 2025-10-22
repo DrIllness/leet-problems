133d531e29501c19c97532d430a0feb881720dba
Eugen Dryl <pickthepill@gmail.com>
2025-10-22 17:40:19 +0300

    NumberOf1Bits

---
+++ src/main/kotlin/NumberOf1Bits.kt
@@ -0,0 +1,65 @@
+/**
+ *
+ * 191. Number of 1 Bits
+ * Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: n = 11
+ *
+ * Output: 3
+ *
+ * Explanation:
+ *
+ * The input binary string 1011 has a total of three set bits.
+ *
+ * Example 2:
+ *
+ * Input: n = 128
+ *
+ * Output: 1
+ *
+ * Explanation:
+ *
+ * The input binary string 10000000 has a total of one set bit.
+ *
+ * Example 3:
+ *
+ * Input: n = 2147483645
+ *
+ * Output: 30
+ *
+ * Explanation:
+ *
+ * The input binary string 1111111111111111111111111111101 has a total of thirty set bits.
+ *
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= n <= 231 - 1
+ *
+ *
+ * Follow up: If this function is called many times, how would you optimize it?
+ *
+ * */
+
+fun main() {
+    println(hammingWeight(11))
+    println(hammingWeight(128))
+    println(hammingWeight(2147483645))
+}
+
+fun hammingWeight(n: Int): Int {
+    var accum = 0
+    var n1 = n
+
+    while (n1 > 0) {
+        if (n1 % 2 > 0) accum++
+
+        n1 /= 2
+    }
+    return accum
+}
