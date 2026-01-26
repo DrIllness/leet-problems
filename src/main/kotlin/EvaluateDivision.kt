c621e973f82201b4a973352d54f277b7616b2460
Eugen Dryl <pickthepill@gmail.com>
2026-01-26 13:03:03 +0200

    EvaluateDivision

---
+++ src/main/kotlin/EvaluateDivision.kt
@@ -0,0 +1,139 @@
+/**
+ * 399. Evaluate Division
+ * Medium
+ * Topics
+ * premium lock icon
+ * Companies
+ * Hint
+ * You are given an array of variable pairs equations and an array of real numbers values,
+ * where equations[i] = [Ai, Bi] and values[i] represent the equation Ai / Bi = values[i]. Each Ai or Bi is a string that represents a single variable.
+ *
+ * You are also given some queries, where queries[j] = [Cj, Dj] represents the jth query where you must find
+ * the answer for Cj / Dj = ?.
+ *
+ * Return the answers to all queries. If a single answer cannot be determined, return -1.0.
+ *
+ * Note: The input is always valid. You may assume that evaluating the queries will not result in division by zero and
+ * that there is no contradiction.
+ *
+ * Note: The variables that do not occur in the list of equations are undefined, so the answer cannot be determined for
+ * them.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: equations = [["a","b"],["b","c"]], values = [2.0,3.0], queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+ * Output: [6.00000,0.50000,-1.00000,1.00000,-1.00000]
+ * Explanation:
+ * Given: a / b = 2.0, b / c = 3.0
+ * queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ?
+ * return: [6.0, 0.5, -1.0, 1.0, -1.0 ]
+ * note: x is undefined => -1.0
+ * Example 2:
+ *
+ * Input: equations = [["a","b"],["b","c"],["bc","cd"]], values = [1.5,2.5,5.0], queries = [["a","c"],["c","b"],["bc","cd"],["cd","bc"]]
+ * Output: [3.75000,0.40000,5.00000,0.20000]
+ * Example 3:
+ *
+ * Input: equations = [["a","b"]], values = [0.5], queries = [["a","b"],["b","a"],["a","c"],["x","y"]]
+ * Output: [0.50000,2.00000,-1.00000,-1.00000]
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= equations.length <= 20
+ * equations[i].length == 2
+ * 1 <= Ai.length, Bi.length <= 5
+ * values.length == equations.length
+ * 0.0 < values[i] <= 20.0
+ * 1 <= queries.length <= 20
+ * queries[i].length == 2
+ * 1 <= Cj.length, Dj.length <= 5
+ * Ai, Bi, Cj, Dj consist of lower case English letters and digits.
+ *
+ * */
+
+fun main() {
+    calcEquation(
+        listOf(
+            listOf(
+                "a", "b"
+            ),
+            listOf(
+                "b", "c"
+            )
+        ),
+        doubleArrayOf(
+            2.0,
+            3.0
+        ),
+        listOf(
+            listOf(
+                "a", "c"
+            ),
+            listOf(
+                "b", "a"
+            ),
+            listOf(
+                "a", "e"
+            )
+        )
+    )
+}
+
+fun calcEquation(equations: List<List<String>>, values: DoubleArray, queries: List<List<String>>): DoubleArray {
+    if (equations.isEmpty() || queries.isEmpty() || values.isEmpty()) emptyArray<DoubleArray>()
+
+    val response = DoubleArray(queries.size)
+    // populate weighted graph
+    val divisionResults = hashMapOf<String, HashMap<String, Double>>()
+    var i = 0
+    while (i < equations.size) {
+        val first = equations[i][0]
+        val second = equations[i][1]
+        if (divisionResults[first] == null) {
+            divisionResults[first] = hashMapOf()
+        }
+        if (divisionResults[second] == null) {
+            divisionResults[second] = hashMapOf()
+        }
+
+        divisionResults[first]!![second] = values[i]
+        divisionResults[second]!![first] = 1.0.div(values[i])
+
+        i++
+    }
+
+    val visitedNodes = hashSetOf<String>()
+    fun findWay(start: String, end: String): Double {
+        if (start == end) return 1.0
+        visitedNodes.add(start)
+        val neighbors = divisionResults[start] ?: return -1.0
+
+        for ((neighbor, weight) in neighbors) {
+            if (visitedNodes.contains(neighbor)) continue
+
+            val result = findWay(neighbor, end)
+            if (result != -1.0) {
+                return weight * result
+            }
+        }
+        return -1.0
+
+    }
+    i = 0
+    while (i < queries.size) {
+        val start = queries[i][0]
+        val end = queries[i][1]
+        if (divisionResults.containsKey(start).not() || divisionResults.containsKey(end).not()) {
+            response[i++] = -1.0
+            continue
+        }
+
+        response[i++] = findWay(start, end)
+        visitedNodes.clear()
+    }
+
+    return response
+}
